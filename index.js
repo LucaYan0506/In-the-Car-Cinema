c0270414141174b679e26776dfee316f9fbedc95
LucaYan0506 <83918638+LucaYan0506@users.noreply.github.com>
2021-11-15 09:57:15 +0000

    fix bug 5

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ function check(){
     for (var i = 0; i < ls.length;i++){
         var location = document.getElementById(ls[i]);
 
-        if (location.getBoundingClientRect().bottom == 0){
-            console.log(location.getBoundingClientRect().bottom + ls[i]);
+        if (location.getBoundingClientRect().top <= 10 & location.getBoundingClientRect().top >= 0){
+            console.log(location.getBoundingClientRect().top + ls[i]);
             document.getElementById(ls[i] + "-link").style.color = "white";
         }
         else{
             document.getElementById(ls[i] + "-link").style.color = ""
         }
     }
+
 
 
 }
